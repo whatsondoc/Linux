144aa15c6c893994f2d1b4b49fa72afc67079cbc
whatsondoc <30378443+whatsondoc@users.noreply.github.com>
2018-04-05 15:45:31 -0400

    ++

--- linux_russian_roulette.sh
+++ linux_russian_roulette.sh
@@ -1,5 +1,7 @@
 ﻿#!/bin/bash
 
+####### THIS IS A SAMPLE SCRIPT TO DEMONSTRATE A SMALL LEARNING EXERCISE ---- THIS HAS THE POTENTIAL TO REDUCE A SYSTEM TO VIRTUALLY NOTHING #######
+
 # Checking for root - no point in playing in sandboxes...
 if [[ $(id -u) != 0 ]]; then /bin/echo "
 Please run as root to play properly...
