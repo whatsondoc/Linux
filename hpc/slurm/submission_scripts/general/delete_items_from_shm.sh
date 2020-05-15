65e11380bcd8e1250cc6cc9b3f5993b639088529
Ben Watson <ben.watson@zenuity.com>
2020-05-15 12:31:06 +0200

    Adding a script to locate and delete items a user owns from a specific directory (default is a system's shared memory).

---
+++ hpc/slurm/submission_scripts/general/delete_items_from_shm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Cleaning up all items a user owns from shared memory
+
+function setup_env() {
+	SELECT_DIRECTORY="/dev/shm"
+	SELECT_USER="${USER}"
+
+	echo "Date	: $(date)"
+	echo "Path	: ${SELECT_DIRECTORY}"
+        echo "Owner	: ${SELECT_USER}"
+        echo "Total	: $(find ${SELECT_DIRECTORY} -user ${SELECT_USER} | wc -l)"
+}
+
+function delete_contents() {
+	echo ">>> Deleting items"
+	find ${SELECT_DIRECTORY} -user ${SELECT_USER} -delete
+	
+	echo
+	echo "Remaining items	: $(find ${SELECT_DIRECTORY} -user ${SELECT_USER} | wc -l)"
+}
+
+#-----------------------------------------------------------------------------------------
+
+echo
+	setup_env
+echo
+	delete_contents
+echo
