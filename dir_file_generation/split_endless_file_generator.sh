b43970b0975b6d35bf7e849fd98306ab0a390664
Ben Watson <ben.watson@datacore.com>
2021-02-19 11:55:07 +0100

    Adjusting how the target directory and source file are specified and checked

--- dir_file_generation/split_endless_file_generator.sh
+++ dir_file_generation/split_endless_file_generator.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-TGT_DIR="/path/to/directory/on/target/filesystem"
+#TGT_DIR="/path/to/directory/on/target/filesystem"
+TGT_DIR="${1}"
+if      [[ ! -d ${1} ]]
+then    echo "Cannot enumerate target directory [first positional argument to the script]: ${1}"
+        exit 1
+else    SOURCE_FILE="${TGT_DIR}/source_file.txt"
+fi
 
-SOURCE_FILE=$(for i in {0..999}; do echo "Split hey, split ho, it's off to work we go..." >> ${TGT_DIR}/source_file.txt)
+for i in {0..999}; do echo "Split hey, split ho, it's off to work we go..." >> ${SOURCE_FILE}
 
 cd ${TGT_DIR}
 
 while true
 do
-    split --lines=1 ${TGT_DIR}/source_file.txt ${TGT_DIR}/prefix_${RANDOM}
+    split --lines=1 ${SOURCE_FILE} ${TGT_DIR}/prefix_${RANDOM}
 done
 
 echo "Complete"     # This is, of course, fool's gold... 
